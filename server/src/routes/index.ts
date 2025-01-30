4d09af78d4584638ae46bea4d6df80ad6306f9fc
Marko Yanovych <markoyanovych123@gmail.com>
2025-01-30 22:20:28 +0200

    create server api

---
+++ server/src/routes/index.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express from 'express';
+import MovieController from '../controllers/MovieController';
+
+const router = express.Router();
+
+router.get('/api/movies', MovieController.getMovies as any);
+router.get('/api/movies/:id', MovieController.getMovie as any);
+router.post('/api/movies', MovieController.createMovie as any);
+router.put('/api/movies/:id', MovieController.updateMovie as any);
+router.put(
+  '/api/movies/:id/toggle-favourite',
+  MovieController.toggleIsFavourite as any,
+);
+router.delete('/api/movies/:id', MovieController.deleteMovie as any);
+
+export default router;
