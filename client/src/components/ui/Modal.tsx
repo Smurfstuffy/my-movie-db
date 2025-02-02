92f68dc582a8170ea820d523f46d6583b9c05b47
Marko Yanovych <markoyanovych123@gmail.com>
2025-02-02 02:19:20 +0200

    fix z axis modal issue

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -7,7 +7,7 @@ const Modal: FC<ModalProps> = ({open, onClose, children}) => {
   return (
     <div
       onClick={onClose}
-      className={`fixed inset-0 flex justify-center items-center
+      className={`fixed inset-0 z-50 flex justify-center items-center
          transition-colors ${open ? 'visible bg-black/20' : 'invisible'}`}
     >
       <div
